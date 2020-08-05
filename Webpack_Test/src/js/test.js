1a72bda482b5cb9ed4650de2f78e45d7961a98cb
DESKTOP-AG1VF4J\86187 <2863279332@qq.com>
2020-08-05 16:32:20 +0800

    【webpack第五节课】 1、安装第三方库 2、引入使用第三方库 3、打包第三方库

--- Webpack_Test/src/js/test.js
+++ Webpack_Test/src/js/test.js
@@ -1,6 +1,12 @@
 import {str} from "./string";
 import '!style-loader!css-loader!../css/style.css'
+import $ from 'jquery'
+/*import 'bootstrap/dist/js/bootstrap.min'
+import 'layui/dist/layui'*/
 
 document.write(str);
 
 document.write(`<div></div>`);
+$('div').click(function () {
+    alert("本省");
+})
